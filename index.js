6df013dbff7b1755c551645ac41f114696d65b6f
unknown <rishisutar183@gmail.com>
2023-04-28 20:20:19 +0530

    Edited index.js mongodb

--- index.js
+++ index.js
@@ -7,11 +7,50 @@ require('dotenv').config({path:'views/.env'});
 
 const PORT = process.env.PORT || 8080;
 
+// const {MongoClient} = require('mongodb');
 
 const mongoose = require('mongoose');
+
 const uri = `mongodb+srv://${process.env.NAME}:${process.env.PASS}@cluster0.kaq6g3b.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose.connect(uri);
+
+mongoose.connect(
+    uri,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+  );
+
+
+// mongoose.connect(uri);
+
+
+// async function main(){
+//     /**
+//      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+//      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+//      */
+//     const uri =`mongodb+srv://${process.env.NAME}:${process.env.PASS}@cluster0.kaq6g3b.mongodb.net/?retryWrites=true&w=majority`;
+ 
+
+//     const client = new MongoClient(uri);
+ 
+//     try {
+//         // Connect to the MongoDB cluster
+//         await client.connect();
+ 
+//         // Make the appropriate DB calls
+//         await  listDatabases(client);
+ 
+//     } catch (e) {
+//         console.error(e);
+//     } finally {
+//         await client.close();
+//     }
+// }
+
+// main().catch(console.error);
 
 app.use(express.static(path.join('views')));
 app.use(express.static(path.join('assets')));
